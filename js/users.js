88087dbee47aafd89850341040afa19059ba7f02
sergiojanko <janko.sergio1998@gmail.com>
2021-08-26 14:26:33 +0200

    layout

---
+++ js/users.js
@@ -0,0 +1,22 @@
+const user = {
+  name: 'Nome Utente',
+  avatar: '_io',
+};
+const contacts = [
+  {
+    name: 'Michele',
+    avatar: '_1',
+  },
+  {
+    name: 'Fabio',
+    avatar: '_2',
+  },
+  {
+    name: 'Samuele',
+    avatar: '_3',
+  },
+  {
+    name: 'Luisa',
+    avatar: '_4',
+  },
+];
